27f1213bb2374cd4664e9c07610d8fe3f3f9717d
Amekare <amekare@gmail.com>
2018-06-14 22:48:24 +0000

    correcciones server

--- world_cup/urls.py
+++ world_cup/urls.py
@@ -14,12 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+import dashboard
+from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
-    path('dashboard/', include('dashboard.urls')),
+    url(r'^$', views.index, name='index'),
+    url(r'^admin/', admin.site.urls),
+    url(r'^dashboard/', include('dashboard.urls')),
+
+
 
 ]
