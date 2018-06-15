bb61f48540b186111579a968a8567a8cf425d81c
Andrea Torres <andrea.torres@ucr.ac.cr>
2018-06-14 20:31:01 -0600

    prueba de img en static

--- world_cup/urls.py
+++ world_cup/urls.py
@@ -27,4 +27,4 @@ urlpatterns = [
     url(r'^dashboard/', include('dashboard.urls')),
 
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
