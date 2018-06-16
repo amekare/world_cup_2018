8b24d62c71295ef28afc78b4ba3307c6ff7ff086
Amekare <amekare@gmail.com>
2018-06-16 03:32:59 +0000

    se aplico actualizacion de modelo de apuesta

--- world_cup/settings.py
+++ world_cup/settings.py
@@ -125,10 +125,10 @@ STATIC_URL = '/static/'
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static/"),
-    '/home/amekare/world_cup_2018/static/',
-]
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, "static/"),
+#    '/home/amekare/world_cup_2018/static/',
+#]
 
 
 
