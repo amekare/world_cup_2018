40356bb460b3312708bf90e67328160c4f2c291f
Andrea Torres <andrea.torres@ucr.ac.cr>
2018-06-15 22:48:55 -0600

    visualizacion individual de las apuestas

--- dashboard/templatetags/custom_tags.py
+++ dashboard/templatetags/custom_tags.py
@@ -1,5 +1,4 @@
-from dashboard.models import Gambler
-from django.template.defaultfilters import stringfilter
+from dashboard.models import Gambler, Bet
 from django import template
 
 register = template.Library()
@@ -10,5 +9,14 @@ def total_points(value, arg):
     gambler = Gambler.objects.get(name=arg)
     points = gambler.points_semi + gambler.points_score + gambler.points_final + gambler.points_8vo+gambler.points_4vo + gambler.points_3er + gambler.points_result
     return str(points)
+
+@register.filter()
+def result(value, arg):
+    try:
+        bet = Bet.objects.get(match=arg, source__name="Oficial")
+    except Bet.DoesNotExist:
+        return 'Sin jugar'
+
+    return str(bet.goals_team1) + " - " + str(bet.goals_team2)
 
 
