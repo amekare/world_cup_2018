7eed544951001550e070daefa607e4eb265d943e
Amekare <amekare@gmail.com>
2018-07-07 18:52:50 +0000

    actualizacion de datos y cambio de signal

--- dashboard/signals.py
+++ dashboard/signals.py
@@ -1,6 +1,6 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from dashboard.views import update_first_round
+from dashboard.views import update_gamblers, update_gamblers2
 
 from dashboard.models import Bet
 
@@ -9,5 +9,7 @@ from dashboard.models import Bet
 def scores_updates(sender, **kwargs):
     bet = kwargs['instance']
     if bet.source.name == 'Oficial':
-        update_first_round(bet.match.stage)
+        #update_first_round(bet.match.stage)
+        update_gamblers()
+        update_gamblers2()
 
